dc9eecc64414f6ca61b00677c28a6c4355338b95
Ashu4999 <ashutoshgorke77@gmail.com>
2024-07-24 17:11:27 +0530

    lesson 02 complete

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,16 @@
 "use strict";
-let username = "John";
-console.log(username);
-let a = 12;
-let b = 6;
-let c = 2;
-console.log(a / b);
-console.log(b * c);
+let myName = "John";
+let noOfDays = 30;
+let isLoading = false;
+let film;
+let code = 404;
+myName = "Dave";
+film = "Harry Potter";
+noOfDays = 31;
+isLoading = false;
+code = "404";
+const sum = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(sum(4, 5));
+let re = /\w+g/;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
-let username = "Barik";
-console.log(username);
+let myName: string = "John";
+let noOfDays: number = 30;
+let isLoading: boolean = false;
+let film: any;
+let code: string | number = 404;
 
-let a: number = 12;
-let b: number = 6;
-let c: number = 2;
+myName = "Dave";
+film = "Harry Potter";
+noOfDays = 31;
+isLoading = false;
+code = "404";
 
-console.log(a / b);
-console.log(b * c);
+const sum = (num1: number, num2: number) => {
+    return num1 + num2;
+};
+
+console.log(sum(4, 5));
+
+let re: RegExp = /\w+g/;
